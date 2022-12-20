e76a9765be9d4fdfdea8f7022828993990b2aa48
carlos <carlossibaja160715@gmail.com>
2022-12-19 19:07:21 -0500

    actualizarhoocks

--- context/GlobalContext.tsx
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-import { GlobalState } from "../interfaces/interfaces";
-
-
-export type GlobalContextProps = {
-    globalState : GlobalState
-}
-
-export const GlobalContext = createContext<GlobalContextProps>({} as GlobalContextProps )
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,14 @@ import type { NextPage } from 'next'
 import React, { useEffect, useState} from 'react'
 import { Menu } from '../components/menu/Menu'
 import { Productos } from '../components/productos/Productos'
-import { GlobalProvider } from '../context/GlobalProvider'
 
 const Home: NextPage = () => {
 
   return (
-    <GlobalProvider>
     <div>
       <Menu />
       <Productos/>
     </div>
-    </GlobalProvider>
   )
 }
 
